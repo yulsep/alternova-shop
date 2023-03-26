db20f4b7d01901ef257ba32dbbfa7fe20f04ce07
yulsep <yulimarcela99@gmail.com>
2023-03-25 21:49:57 -0500

    json data update

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,3 +1,5 @@
+/* In case we need to consume any backend
+
 import { API } from "../api";
 import { BASE_URL } from "../utils";
 
@@ -7,5 +9,18 @@ export async function fetchAllProducts() {
     return products;
   } catch (error) {
     throw new Error(`Problems to get data...`);
+  }
+}
+ */
+
+import productsMock from "../mocks/productsMock.json";
+
+export async function fetchAllProducts() {
+  try {
+    const products = await import("../mocks/productsMock.json");
+    return products.default;
+  } catch (error) {
+    console.error(`Problems to get data: ${error.message}`);
+    return productsMock;
   }
 }
